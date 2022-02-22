0dbe9a96f022d31e6a1f232d28938c8f4e8ff2cf
Max <lucas97lopes@hotmail.com>
2022-02-22 13:38:22 -0300

    feat: webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,7 +23,9 @@ export const config = {
 }
 
 const relevantEvent = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -44,20 +46,32 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvent.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscriptions = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscriptions.id ?? '',
+              subscriptions.customer?.toString() ?? '',
+              false
+            )
+
+            break
           case 'checkout.session.completed':
 
             const checkoutSession = event.data.object as Stripe.Checkout.Session
 
             await saveSubscription(
               checkoutSession.subscription?.toString() ?? '',
-              checkoutSession.customer?.toString() ?? ''
+              checkoutSession.customer?.toString() ?? '',
+              true
             )
 
             break;
           default:
             throw new Error('Unhandled event.');
         }
-      } catch(err) {
+      } catch (err) {
         return res.json({ error: 'Webhook handler failed.' })
       }
     }
